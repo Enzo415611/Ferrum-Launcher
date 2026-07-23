de51d523cf4434c8749a0bc7785ae704f7b62f11
Enzo Queiroz Costa <enzo415611@gmail.com>
2026-07-23 13:56:30 -0300

    add crate dioxus_style

--- src/view/login.rs
+++ src/view/login.rs
@@ -1,34 +1,29 @@
-use dioxus::prelude::*;
+use dioxus::{document::Style, html::u::style, prelude::{AssetVariant::Css, *}};
 use dioxus_primitives::select::SelectGroup;
-
+use dioxus_style::{scoped_style, with_css};
 use crate::{Route};
 
-#[derive(Debug, Clone, )]
-struct Users {
-
-}
-
 #[component]
+#[with_css(css, "src/view/css/login.css")]
 pub fn Login() -> Element {
     let navigator = use_navigator();
     let mut border_color = use_signal(|| "#282A36") ;
+    
     rsx! {
-        div {
-            class: "login",
+        div { class: css::login,
             div {
-                class: "login-center",
+                class: css::login_center,
                 border_color: border_color.read().to_string(),
                 onmouseenter: move |_| border_color.set("#7E57C2"),
                 onmouseleave: move |_| border_color.set("#282A36"),
-                button {
-                    "nome da conta"
-                }
-                div {
-                    class: "center",
+                button { "nome da conta" }
+                div { class: css::center,
                     button { "Online" }
-                    button { "Offline"}
+                    button { "Offline" }
                 }
             }
         }
     }
+    
+
 }
